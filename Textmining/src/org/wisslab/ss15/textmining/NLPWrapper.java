0c6a8001c2ef2a0a5b411a5ef42ad48289bf009a
Kai Eckert <eckert@hdm-stuttgart.de>
2016-05-12 12:29:52 +0200

    Open NLP Integration, Ausgabe aller Personen und Orte pro Speaker.

--- Textmining/src/org/wisslab/ss15/textmining/NLPWrapper.java
+++ Textmining/src/org/wisslab/ss15/textmining/NLPWrapper.java
@@ -5,9 +5,21 @@
  */
 package org.wisslab.ss15.textmining;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Pattern;
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.sentdetect.SentenceDetector;
+import opennlp.tools.sentdetect.SentenceDetectorME;
+import opennlp.tools.sentdetect.SentenceModel;
+import opennlp.tools.tokenize.Tokenizer;
+import opennlp.tools.tokenize.TokenizerME;
+import opennlp.tools.tokenize.TokenizerModel;
+import opennlp.tools.util.Span;
 
 /**
  *
@@ -15,15 +27,29 @@ import java.util.regex.Pattern;
  */
 public class NLPWrapper {
 
-    // Tokenizer tokenizer = null;
+    Tokenizer tokenizer = null;
+    SentenceDetector sentenceDetector = null;
+    NameFinderME personFinder = null;
+    NameFinderME locationFinder = null;
 
     public NLPWrapper() {
-        /*
         InputStream modelIn = null;
         try {
             modelIn = new FileInputStream("en-token.bin");
             TokenizerModel model = new TokenizerModel(modelIn);
             tokenizer = new TokenizerME(model);
+            modelIn.close();
+            modelIn = new FileInputStream("en-sent.bin");
+            SentenceModel sentModel = new SentenceModel(modelIn);
+            sentenceDetector = new SentenceDetectorME(sentModel);
+            modelIn.close();
+            modelIn = new FileInputStream("en-ner-location.bin");
+            TokenNameFinderModel locationModel = new TokenNameFinderModel(modelIn);
+            locationFinder = new NameFinderME(locationModel);
+            modelIn.close();
+            modelIn = new FileInputStream("en-ner-person.bin");
+            TokenNameFinderModel personModel = new TokenNameFinderModel(modelIn);
+            personFinder = new NameFinderME(personModel);
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
@@ -36,16 +62,53 @@ public class NLPWrapper {
                 }
             }
         }
-        */
-
+    
     }
 
+    public List<String> splitSentences(String input) {
+        return Arrays.asList(sentenceDetector.sentDetect(input));
+    }
+
+    public List<String> findPersons(List<String> tokens) {
+        Span[] spans = personFinder.find((String[]) tokens.toArray());
+        List<String> result = new ArrayList<>();
+        for (Span s: spans) {
+            StringBuilder name = new StringBuilder();
+            for (int i = s.getStart();i<s.getEnd();i++) {
+                if (i>s.getStart()) {
+                    name.append(" ");
+                }
+                name.append(tokens.get(i));
+            }
+            result.add(name.toString());
+        }
+        return result;
+    }
+    
+
+    public List<String> findLocations(List<String> tokens) {
+        Span[] spans = locationFinder.find( (String[]) tokens.toArray() );
+        List<String> result = new ArrayList<>();
+        for (Span s: spans) {
+            StringBuilder name = new StringBuilder();
+            for (int i = s.getStart();i<s.getEnd();i++) {
+                if (i>s.getStart()) {
+                    name.append(" ");
+                }
+                name.append(tokens.get(i));
+            }
+            result.add(name.toString());
+        }
+        return result;
+    }
+    
+
+    
     // Unser Behelfs-Tokenizer aus der Vorlesung
     public List<String> tokenize(String input) {
-        List<String> result = new ArrayList<>();
-        /* result.addAll(Arrays.asList(tokenizer.tokenize(input)));
-         return result;
-         */
+        return Arrays.asList(tokenizer.tokenize(input));
+        
+        /*
         Pattern p1 = Pattern.compile("[a-zA-Z']+");
         for (String word : input.split(" ")) {
             // Das ist quasi Tokenization, dafür gibt es Spezialbibliotheken
@@ -60,7 +123,7 @@ public class NLPWrapper {
             result.add(word);
         }
         return result;
-
+        */
     }
 
 }
--- Textmining/src/org/wisslab/ss15/textmining/tm.java
+++ Textmining/src/org/wisslab/ss15/textmining/tm.java
@@ -8,7 +8,9 @@ package org.wisslab.ss15.textmining;
 import org.wisslab.ss15.textmining.vectorspace.VectorSpace;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import org.wisslab.ss15.textmining.vectorspace.DocumentPair;
 
 /**
@@ -32,6 +34,29 @@ public class tm {
         // Kleine Rollen rauswerfen...
         int minLength = 0;
 
+        for (Speaker speaker : works.getAllSpeakers()) {
+            Set<String> persons = new HashSet<>();
+            Set<String> locations = new HashSet<>();
+            List<String> sentences = nlp.splitSentences(speaker.getAllText());
+            for (String sentence: sentences) {
+                List<String> tokens = nlp.tokenize(sentence);
+                persons.addAll(nlp.findPersons(tokens));
+                locations.addAll(nlp.findLocations(tokens));
+            }
+            System.out.println(speaker.toString() + " talks about persons: ");
+            for (String name: persons) {
+                System.out.println("   " + name);
+            }
+            System.out.println();
+            System.out.println(speaker.toString() + " talks about locations: ");
+            for (String name: locations) {
+                System.out.println("   " + name);
+            }
+            System.out.println();
+
+        }
+        
+
         // Vector Space erzeugen
         for (Speaker speaker : works.getAllSpeakers()) {
             if (speaker.getAllText().length() < minLength) {
@@ -40,6 +65,8 @@ public class tm {
             vs.addDocument(speaker.toString(), nlp.tokenize(speaker.getAllText()));
         }
 
+        
+        
         System.out.println("Calculating pairs:");
 
         List<DocumentPair> pairs = new ArrayList<>();
